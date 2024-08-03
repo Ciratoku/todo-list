22b739eff78bb952ad3bbe819a5ea82fedc236d4
Ciratoku <edissonman255@gmail.com>
2024-08-03 20:33:00 +0300

    handle radio and sort

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,6 +5,8 @@ import ModalForm from "./ModalForm";
 import { useState } from "react";
 
 const Table = ({ todos, setTodos, updateTodos }) => {
+  const [radio, setRadio] = useState("all");
+  const [sort, setSort] = useState("def");
   const [modal, setModal] = useState(false);
   const [currentTodo, setCurrentTodo] = useState({});
   const handleClick = (e) => {
@@ -25,24 +27,47 @@ const Table = ({ todos, setTodos, updateTodos }) => {
     let tds = JSON.parse(localStorage.getItem("todos"));
     if (mode == "compl") tds = tds.filter((todo) => todo.completed);
     else if (mode == "notcompl") tds = tds.filter((todo) => !todo.completed);
+    setRadio(mode);
     setTodos(tds);
+  };
+  const handleSort = (e) => {
+    const mode = e.target.value;
+    setSort(mode);
+    let sortedTodos = [...todos];
+    mode == "asc"
+      ? sortedTodos.sort((a, b) => new Date(a.date) - new Date(b.date))
+      : sortedTodos.sort((a, b) => -(new Date(a.date) - new Date(b.date)));
+    setTodos(sortedTodos);
   };
   return (
     <>
       <div className="sortCompl">
         <p>
-          <input name="dzen" type="radio" value="all" onClick={handleRadio} />
+          <input
+            name="all"
+            type="radio"
+            value="all"
+            checked={radio == "all" ? true : false}
+            onClick={handleRadio}
+          />
           Все
         </p>
         <p>
-          <input name="dzen" type="radio" value="compl" onClick={handleRadio} />
+          <input
+            name="compl"
+            type="radio"
+            value="compl"
+            checked={radio == "compl" ? true : false}
+            onClick={handleRadio}
+          />
           Завершенные
         </p>
         <p>
           <input
-            name="dzen"
+            name="notcompl"
             type="radio"
             value="notcompl"
+            checked={radio == "notcompl" ? true : false}
             onClick={handleRadio}
           />
           Незавершенные
@@ -52,7 +77,14 @@ const Table = ({ todos, setTodos, updateTodos }) => {
         <thead>
           <tr>
             <th>Задача</th>
-            <th>Дата выполнения</th>
+            <th>
+              Дата выполнения
+              <select className="control" value={sort} onChange={handleSort}>
+                <option value="def">без сорт.</option>
+                <option value="desc">по убыв.</option>
+                <option value="asc">по возраст.</option>
+              </select>
+            </th>
             <th>Описание</th>
             <th>Действия</th>
           </tr>
